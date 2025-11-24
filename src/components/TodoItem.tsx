9fbda980b21d09393babee0d7944d158e1dbafd5
Ayush Kumar Nath <ayushkumar.nath25@gmail.com>
2025-11-24 05:08:50 +0000

    implements task check-off feature

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,42 @@
+import React, { useContext, useState } from "react";
 import type { TodoItemType } from "../types/TodoItemType.ts";
 import TodoItemDelete from "./TodoItemDelete.tsx";
 import TodoItemEdit from "./TodoItemEdit.tsx";
+import { TodosContext } from "../contexts/TodosContext.ts";
 
 interface TodoItemProps {
   item: TodoItemType;
 }
 
 export default function TodoItem({ item }: TodoItemProps) {
+  const [checked, setChecked] = useState(false);
+  const { todos, setTodos } = useContext(TodosContext);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+    const newTodos = todos.map((todo) =>
+      item.id === todo.id
+        ? { ...todo, completed: e.target.checked }
+        : { ...todo },
+    );
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <input className="justify-self-start" type="checkbox" id={`${item.id}`} />
-      <label htmlFor={`${item.id}`}>{item.text}</label>
+      <input
+        className="justify-self-start"
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+        id={`${item.id}`}
+      />
+      <label
+        className={`${checked ? "line-through" : ""}`}
+        htmlFor={`${item.id}`}
+      >
+        {item.text}
+      </label>
       <div className="flex justify-between items-center">
         <TodoItemEdit />
         <TodoItemDelete />
